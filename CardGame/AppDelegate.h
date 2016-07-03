0b6bf657aa98f730dbf68c5f368d7a697fd57abd
Luis Arias <lugearma@gmail.com>
2016-07-03 12:17:03 -0500

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by Luis Arias on 03/07/16.
+//  Copyright © 2016 Luis Arias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CardGame
+//
+//  Created by Luis Arias on 03/07/16.
+//  Copyright © 2016 Luis Arias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
