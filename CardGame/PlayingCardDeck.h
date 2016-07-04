f7d28a03e540cd565af67b62b0b12241443586e1
Luis Arias <lugearma@gmail.com>
2016-07-03 20:05:03 -0500

    Added PlayingCardDeck class

---
+++ CardGame/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  CardGame
+//
+//  Created by Luis Arias on 03/07/16.
+//  Copyright © 2016 Luis Arias. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ CardGame/PlayingCardDeck.m
@@ -0,0 +1,33 @@
+//
+//  PlayingCardDeck.m
+//  CardGame
+//
+//  Created by Luis Arias on 03/07/16.
+//  Copyright © 2016 Luis Arias. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@implementation PlayingCardDeck
+
+- (instancetype)init {
+    self = [super init];
+    
+    if (self) {
+        
+        for (NSString *suit in [PlayingCard validSuits]) {
+            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+                [self addCard:card];
+            }
+        }
+        
+    }
+    
+    return self;
+}
+
+@end
