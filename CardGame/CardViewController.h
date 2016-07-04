722314924b25dac38dfced31c040a01dbbcd5ef1
Luis Arias <lugearma@gmail.com>
2016-07-03 20:28:54 -0500

    Added background and card to view

---
+++ CardGame/CardViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardViewController.h
+//  CardGame
+//
+//  Created by Luis Arias on 03/07/16.
+//  Copyright © 2016 Luis Arias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardViewController : UIViewController
+
+@end
---
+++ CardGame/CardViewController.m
@@ -0,0 +1,26 @@
+//
+//  CardViewController.m
+//  CardGame
+//
+//  Created by Luis Arias on 03/07/16.
+//  Copyright © 2016 Luis Arias. All rights reserved.
+//
+
+#import "CardViewController.h"
+
+@interface CardViewController ()
+
+@end
+
+@implementation CardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    
+}
+
+@end
